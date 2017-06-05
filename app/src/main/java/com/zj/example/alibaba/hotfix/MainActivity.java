137769b1367f8a8359894bfc87fb9cd6e01f1716
zhengjiong <zhengjiong2006@gmail.com>
2017-06-05 23:27:37 +0800

    feat<hotfix>add demo

--- app/src/main/java/com/zj/example/alibaba/hotfix/MainActivity.java
+++ app/src/main/java/com/zj/example/alibaba/hotfix/MainActivity.java
@@ -1,13 +1,59 @@
 package com.zj.example.alibaba.hotfix;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.taobao.sophix.SophixManager;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int REQUEST_EXTERNAL_STORAGE_PERMISSION = 0;
+    private static final String LOCAO_PATCH_PATH = "/baichuan-hotfix-patch.jar";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        requestExternalStoragePermission();
+
+        TextView title = (TextView) findViewById(R.id.title);
+        title.setText("11111111111111");
+
+        findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SophixManager.getInstance().queryAndLoadNewPatch();
+            }
+        });
+    }
+
+    /**
+     * 如果本地补丁放在了外部存储卡中, 6.0以上需要申请读外部存储卡权限才能够使用. 应用内部存储则不受影响
+     */
+
+    private void requestExternalStoragePermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                    REQUEST_EXTERNAL_STORAGE_PERMISSION);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_EXTERNAL_STORAGE_PERMISSION:
+                if (grantResults.length <= 0 || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                    //updateConsole("local external storage patch is invalid as not read external storage permission");
+                }
+                break;
+            default:
+        }
     }
 }
